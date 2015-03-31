b18f62229b772d0314d6edec392cb70c07fdc0cc
torreyn <torrey@nommesen.com>
2015-03-31 04:55:50 -0700

    got minute the indicator ACTUALLY working. Though now the timing is a bit off

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,7 @@
 /* 
 /*  The red button stops the timer sequence
 /* 
-/*  The white button resets the timer sequence - White button removed
+/*  (The white button resets the timer sequence - White button removed)
 /* 
 /*  The question mark at the top goes to a help page
 /* 
@@ -27,6 +27,7 @@ $(document).ready(function () {
 var TimerVar; // function calls countSeconds() every second
 var Secs = 0; // seconds passed
 var Mins = 0; // minutes passed
+var ONE_SECOND = 1000; //**** should be 1000 for 1 second, speed up to 10 or 100 for testing
 
 function beep(beepID) { // takes audio element beepID and plays it "#beep1" or "#beep2"
     $(beepID).get(0).play();
@@ -51,21 +52,20 @@ function visibleAll() {
     }
 }
 
-function blinkMe(n, elem, times) {
-    console.log('blinMe(' + elem + ', ' + times + ') called');
+function blinkMe(n, elem) {
+    console.log('blinkMe(' + elem + '[' + n + ']) called');
     var thing = $(elem)[n];
-    console.log('thing = ' + thing);
-    for (i = 0; i < times; i++) {
+    for (i = 0; i < 29; i++) {
         $(thing).animate({
             opacity: '1'
-        }, 1000);
+        }, ONE_SECOND);
         $(thing).animate({
             opacity: '0.5'
-        }, 1000);
+        }, ONE_SECOND);
     }
     $(thing).animate({
         opacity: '1'
-    }, 1000);
+    }, ONE_SECOND);
 }
 
 function numberCountdown() {
@@ -75,83 +75,140 @@ function numberCountdown() {
 }
 
 function countMinutes() { // called every minute by CountSeconds
-    //	console.log("minute passed: " + Mins);
-
+    console.log("minute passed: " + Mins);
     if (Mins < 10) { // 1st 10 minutes
+        console.log("Mins < 10: " + Mins);
         countBar(0, ".ten");
+        if (Mins < 10) { // Why does this work? Mins jumps the gun? fuck, I don't know/
+            blinkMe(0, ".ten");
+        } else {
+            blinkMe(0, ".two");
+        };
     } else if (Mins >= 10 && Mins < 12) { // 1st 2 minutes
+        console.log("Mins >= 10 && Mins < 12: " + Mins);
         countBar(0, ".two");
+        if (Mins < 12) {
+            blinkMe(0, ".two");
+        } else {
+            blinkMe(1, ".ten");
+        };
     } else if (Mins >= 12 && Mins < 22) { // 2nd 10 minutes
+        console.log("Mins >= 12 && Mins < 22: " + Mins);
         countBar(1, ".ten");
+        if (Mins < 22) {
+            blinkMe(1, ".ten");
+        } else {
+            blinkMe(1, ".two");
+        };
     } else if (Mins >= 22 && Mins < 24) { // 2nd 2 minutes
+        console.log("Mins >= 22 && Mins < 24: " + Mins);
         countBar(1, ".two");
+        if (Mins < 24) {
+            blinkMe(1, ".two");
+        } else {
+            blinkMe(2, ".ten");
+        };
     } else if (Mins >= 24 && Mins < 34) { // 3rd 10 minutes
+        console.log("Mins >= 24 && Mins < 34: " + Mins);
         countBar(2, ".ten");
+        if (Mins < 34) {
+            blinkMe(2, ".ten");
+        } else {
+            blinkMe(2, ".two");
+        };
     } else if (Mins >= 34 && Mins < 36) { // 3rd 2 minutes
+        console.log("Mins >= 34 && Mins < 36: " + Mins);
         countBar(2, ".two");
+        if (Mins < 36) {
+            blinkMe(2, ".two");
+        } else {
+            blinkMe(3, ".ten");
+        };
     } else if (Mins >= 36 && Mins < 46) { // 4th 10 minutes
+        console.log("Mins >= 36 && Mins < 4: " + Mins);
         countBar(3, ".ten");
+        if (Mins < 46) {
+            blinkMe(3, ".ten");
+        } else {
+            blinkMe(3, ".two");
+        };
     } else if (Mins >= 46 && Mins < 48) { // 4th 2 minutes
+        console.log("Mins >= 46 && Mins < 48: " + Mins);
         countBar(3, ".two");
+        if (Mins < 48) {
+            blinkMe(3, ".two");
+        } else {
+            blinkMe(4, ".ten");
+        };
     } else if (Mins >= 48 && Mins < 58) { // 5th 10 minute
+        console.log("Mins >= 48 && Mins < 58: " + Mins);
         countBar(4, ".ten");
+        if (Mins < 58) {
+            blinkMe(4, ".ten");
+        } else {
+            blinkMe(4, ".two");
+        };
     } else if (Mins >= 58 && Mins < 60) { // 5th 2 minutes
+        console.log("Mins >= 58 && Mins < 60: " + Mins);
         countBar(4, ".two");
+        if (Mins < 60) {
+            blinkMe(4, ".two");
+        };
     };
 
     if (Mins == 10) {
         beep("#beep2");
         $($(".ten")[0]).animate({
             opacity: '0'
-        }, 1000);
+        }, ONE_SECOND);
     };
     if (Mins == 12) {
         beep("#beep1");
         $($(".two")[0]).animate({
             opacity: '0'
-        }, 1000);
+        }, ONE_SECOND);
     };
     if (Mins == 22) {
         beep("#beep2");
         $($(".ten")[1]).animate({
             opacity: '0'
-        }, 1000);
+        }, ONE_SECOND);
     };
     if (Mins == 24) {
         beep("#beep1");
         $($(".two")[1]).animate({
             opacity: '0'
-        }, 1000);
+        }, ONE_SECOND);
     };
     if (Mins == 34) {
         beep("#beep2");
         $($(".ten")[2]).animate({
             opacity: '0'
-        }, 1000);
+        }, ONE_SECOND);
     };
     if (Mins == 36) {
         beep("#beep1");
         $($(".two")[2]).animate({
             opacity: '0'
-        }, 1000);
+        }, ONE_SECOND);
     };
     if (Mins == 46) {
         beep("#beep2");
         $($(".ten")[3]).animate({
             opacity: '0'
-        }, 1000);
+        }, ONE_SECOND);
     };
     if (Mins == 48) {
         beep("#beep1");
         $($(".two")[3]).animate({
             opacity: '0'
-        }, 1000);
+        }, ONE_SECOND);
     };
     if (Mins == 58) {
         beep("#beep2");
         $($(".ten")[4]).animate({
             opacity: '0'
-        }, 1000);
+        }, ONE_SECOND);
     };
     if (Mins == 60) {
         beep("#beep1");
@@ -164,7 +221,6 @@ function countBar(n, elem) { // the nth element is effected
     Mins = Mins + 1;
     var minsMod = Mins % 12;
     var bar = $(elem)[n];
-    blinkMe(n, elem, 29);
     if (elem == ".ten") {
         var barHeight = window.tenBarHeight;
         var barSegment = barHeight / 10;
@@ -181,14 +237,14 @@ function countBar(n, elem) { // the nth element is effected
     });
     numberCountdown();
     return barHeight;
-    console.log(barHeight + " - " + barChunk + " = " + (barHeight - barChunk));
+    // console.log(barHeight + " - " + barChunk + " = " + (barHeight - barChunk));
 }
 
 function timerSequenceStart() {
     //	console.log("timerSequenceStart() called");
     TimerVar = setInterval(function () {
         countSeconds()
-    }, 1000); //**** change to 1000 when finished
+    }, ONE_SECOND);
 }
 
 function countSeconds() {
@@ -199,7 +255,7 @@ function countSeconds() {
     // console.log("Secs = " + Secs + " | Secs mod 60 = " + Secs % 60 + " | boxWidth = " + boxWidth);
     $("#timeUnit").animate({
         width: boxSec * (Secs % 60)
-    }, 1000, 'linear'); //**** change to 1000 when finished
+    }, ONE_SECOND, 'linear');
     if (Secs % 60 == 0) {
         countMinutes();
     }
@@ -207,7 +263,7 @@ function countSeconds() {
 
 function start() {
     //	console.log("start() called");
-    blinkMe(0, '.ten', 29);
+    blinkMe(0, '.ten');
     beep("#beep1");
     $("#stop").addClass("click"); // stop button look clickable
     $("#start").removeClass("click"); // start button looks non-clickable
@@ -217,13 +273,15 @@ function start() {
 
 function stop() {
     //	console.log("stop() called");
+    $(".ten").stop(true,true);
+    $(".two").stop(true,true);
     $("#stop").removeClass("click"); // rest buttons look non-clickable
     $("#start").addClass("click"); // start button looks clickable
     clearInterval(TimerVar); // stop timer
     reset();
 }
 
-function reset() { // for optional reset button
+function reset() { // for reset button to maybe be put in a later version
     //	console.log('reset() called')
     $("#timeUnit").clearQueue();
     $("#timeUnit").stop();
@@ -238,7 +296,7 @@ function reset() { // for optional reset button
 }
 
 function helpPage() {
-    console.log('helpPage() called')
+    // console.log('helpPage() called')
     $("#helpPage").css({
         'display': 'block'
     });
@@ -246,7 +304,7 @@ function helpPage() {
 }
 
 function back() {
-    console.log('back() called')
+    // console.log('back() called')
     $("#main").css({
         'display': 'block'
     });
